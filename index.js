d49faa61428b8c4a6c8a4ea8e4e362c39a360d23
Frankely Rafael Díaz Checo <diazfrankely@gmail.com>
2016-01-08 18:41:21 -0500

    add split test cases
    add new test cases for the split function

--- index.js
+++ index.js
@@ -7,5 +7,24 @@ module.exports = {
       return false;
 
     return true;
+  },
+  split: function(vin) {
+    var INDEXES = {
+      ASSEMBLY_PLANT: 9,
+      YEAR: 8,
+      SECURITY_CODE: 7
+    };
+
+    var rawInfo = {
+      madeIn: '',
+      manufacter: '',
+      details: '',
+      securityCode: vin.charAt(INDEXES.SECURITY_CODE),
+      year: vin.charAt(INDEXES.YEAR),
+      assemblyPlant: vin.charAt(INDEXES.ASSEMBLY_PLANT),
+      serialNumber: ''
+    };
+
+    return rawInfo;
   }
 };
--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 var should = require('chai').should();
 var vinDecoder = require('../index');
 var validate = vinDecoder.validate;
+var split = vinDecoder.split;
 
 
 describe('#validate', function(){
@@ -14,5 +15,17 @@ describe('#validate', function(){
 
   it('validate the length to be 17',function(){
     validate('1NXBR32E77Z923602').should.equal(true);
+  });
+});
+
+describe('#split',function(){
+  it('split the vehicle year code',function(){
+    split('1NXBR32E77Z923602').year.should.equal('7');
+  });
+  it('split the vehicle security code',function(){
+    split('1NXBR32E77Z923602').securityCode.should.equal('7');
+  });
+  it('split the vehicle assembly plant code',function(){
+    split('1NXBR32E77Z923602').assemblyPlant.should.equal('Z');
   });
 });
